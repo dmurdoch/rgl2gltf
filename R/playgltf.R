fd595c6a63ccdcddb8d8abd228745d18a3cde870
Duncan Murdoch <murdoch.duncan@gmail.com>
2022-01-07 11:56:55 -0500

    Very close with rigid method, just having a problem with bbox

--- R/playgltf.R
+++ R/playgltf.R
@@ -29,19 +29,56 @@ getAffectedObjects <- function(gltf, method) {
         }
         if (length(weights)) {
           tag <- paste0(meshnum, ":", j)
-          jindex0 <- unique(jindex[weights > 0])
-          if (method == "rigid") {
-            wt <- 0*jindex
-            for (k in seq_along(jindex0)) {
-              wt[k] <- wt[k] + sum(weights[jindex == jindex0[k]])
-            }
-            prim$usejoint <- jindex0[which.max(wt)]
-            mesh$primitives[[j]] <- prim
-            jindex0 <- jindex0[prim$usejoint]
-          }
-          for (ji in jindex0) {
+          jindex_vals <- unique(as.numeric(jindex[weights > 0]))
+
+          for (ji in jindex_vals) {
             joint <- joints[ji + 1]
-            result[joint+1] <- list(c(result[[joint+1]], tag))
+            result[[joint + 1]] <- c(result[[joint + 1]], tag)
+          }
+          if (method == "rigid") {
+            if (is.null(prim$indices))
+              indices <- seq_len(nrow(weights))
+            else
+              indices <- gltf$readAccessor(prim$indices) + 1 # R indices start at 1
+            if (is.null(mode <- prim$mode))
+              mode <- 4
+            primsize <- c("0" = 1, "1" = 2, "4" = 3)[as.character(mode)]
+            if (is.na(primsize))
+              stop("primitive mode ", mode, " not implemented in rigid method.")
+
+            # weights_expanded <- weights[indices,]
+            jindex_expanded <- jindex[indices,]
+
+            # Map jindex values to entries in jindex_vals
+            map <- seq_len(max(jindex_vals) + 1)
+            map[jindex_vals + 1] <- seq_along(jindex_vals)
+
+            weights_expanded <- matrix(0, ncol = length(jindex_vals), nrow = length(indices))
+            for (col in 1:4) {
+              idx <- cbind(seq_along(indices), map[jindex_expanded[, col] + 1])
+              weights_expanded[idx] <- weights_expanded[idx] + weights[indices, col, drop = FALSE]
+            }
+            if (primsize > 1) {
+              for (gp in seq_len(length(indices)/primsize) - 1) {
+                idx <- primsize * gp + 1:primsize
+                weights_expanded[idx,] <-
+                  rep(colMeans(weights_expanded[idx,,drop = FALSE]), each = primsize)
+              }
+            }
+            unique_weights <- unique(weights_expanded)
+            colnames(unique_weights) <- as.character(jindex_vals)
+            rownames(unique_weights) <- paste0(tag, ":", 1:nrow(unique_weights))
+            indices_split <- list()
+            if ((nw <- nrow(unique_weights)) == 1)
+              indices_split <- list(indices)
+            else {
+              indices_split <- vector("list", nw)
+              for (gp in 1:nw)
+                indices_split[[gp]] <- indices[apply(weights_expanded, 1, function(row) all(row == unique_weights[gp,]))]
+            }
+            prim$unique_weights <- unique_weights
+            prim$indices_split <- indices_split
+            mesh$primitives[[j]] <- prim
           }
         }
       }
@@ -66,10 +103,28 @@ getAffectedObjects <- function(gltf, method) {
 
 playgltf <- function(gltf, animation = 0, start = times[1],
                      stop = times[2], times = gltf$timerange(animation),
-                     method = c("wholeScene", "partialScene", "rigid"), ...) {
+                     method = c("wholeScene", "partialScene", "rigid"),
+                     speed = 1, by = NULL, verbose = FALSE, ...) {
+
+  getMatrices <- function(n) {
+    if (n != havenode) {
+      havenode <<- n
+      forward <<- NULL
+      backward <<- NULL
+      node <- gltf$getNode(n)
+      if (!is.null(node$skin)) {
+        skin <- gltf$getSkin(node$skin)
+        forward <<- skin$forward
+        backward <<- gltf$getInverseBindMatrices(skin)
+      }
+    }
+  }
 
   if (animation + 1 > gltf$listCount("animations"))
     stop("Animation not found")
+
+  if (verbose)
+    cat("Initial plot...\n")
 
   method <- match.arg(method)
   time <- start
@@ -81,11 +136,15 @@ playgltf <- function(gltf, animation = 0, start = times[1],
   on.exit(par3d(save))
 
   if (method %in% c("partialScene", "rigid")) {
+    if (verbose)
+      cat("Preparing skeleton...\n")
+    root <- currentSubscene3d()
+
     # Make a list indexed by the node number (+1) of the
     # objects that it affects.  When it changes, those
     # objects will be deleted and then redrawn in the
-    # partialScene method.  In the rigid method, only
-    # the node with the highest total weight will count.
+    # partialScene method.  In the rigid method, this
+    # will be converted to subscenes that need modification
     affectedObjects <- getAffectedObjects(gltf, method)
     allNames <- unique(unlist(affectedObjects))
     containingNodes <- vector("list", length(allNames))
@@ -104,62 +163,73 @@ playgltf <- function(gltf, animation = 0, start = times[1],
   }
 
   if (method == "rigid") {
-      # Put each primitive into a separate subscene
+      # Break each primitive into one or more subscenes
       # under its original one.  Set the userMatrix
-      # for that subscene to match the majority transformation
+      # for each subscene to match the average transformation
       # for its coordinates.
-    wrappers <- containingNodes
+    wrappers <- vector("list", length(containingNodes))
+    names(wrappers) <- allNames <- names(containingNodes)
     havenode <- -1
-    for (tag in names(containingNodes)) {
-      # if (tag == "0:43")
-      #   browser()
+    for (tag in allNames) {
       nodes <- containingNodes[[tag]]
       for (i in seq_along(nodes)) {
         subname <- paste0("subscene", nodes[i])
         nodeid <- res[subname]
-        wrappers[[tag]][i] <- subid <- newSubscene3d(model="modify",
-                                                     viewport = "inherit",
-                                                     projection = "inherit",
-                                                     parent = nodeid)
+
         id <- tagged3d(tag, subscene = nodeid)
-        if (nodes[i] != havenode) {
-          havenode <- nodes[i]
-          forward <- NULL
-          backward <- NULL
-          node <- gltf$getNode(nodes[i])
-          if (!is.null(node$skin)) {
-            skin <- gltf$getSkin(node$skin)
-            forward <- skin$forward
-            backward <- gltf$getInverseBindMatrices(skin)
-          }
-        }
+        getMatrices(nodes[i])
+        node <- gltf$getNode(nodes[i])
         if (!is.null(forward)) {
           prim <- getPrim(gltf, tag)
-          par3d(userMatrix = forward[,,prim$usejoint + 1] %*% backward[,,prim$usejoint + 1],
-                listeners = nodeid,
-                subscene = subid)
-          pop3d(id = id)
-          cat("tag =", tag, " bbox=", par3d("bbox", subscene=nodeid)[1:2], "\n")
+          subids <- numeric(length(prim$indices_split))
+          weights <- prim$unique_weights
+          joints <- as.numeric(colnames(weights))
           newobj <- primToRglobj(prim, node$skin,
-                             gltf = gltf,
-                             defaultmaterial = s$material)
-          newobj$material$tag <- tag
-          useSubscene3d(subid)
-          plot3d(newobj, add=TRUE)
+                                 gltf = gltf,
+                                 defaultmaterial = s$material,
+                                 doTransform = FALSE)
+          tags <- rownames(weights)
+          for (j in seq_along(subids)) {
+            subids[j] <- newSubscene3d(model="modify",
+                                       viewport = "inherit",
+                                       projection = "inherit",
+                                       parent = nodeid)
+            transform <- weightedTransform(joints, weights[j,],
+                                           forward, backward)
+            useSubscene3d(subids[j])
+            par3d(userMatrix = transform,
+                listeners = nodeid)
+            newobj$indices <- prim$indices_split[[j]]
+            newobj$material$tag <- tags[j]
+            newid <- plot3d(newobj, add = TRUE)
+          }
+          pop3d(id = id)
         }
       }
+      wrappers[[tag]] <- subids
     }
   }
 
   clockstart <- Sys.time()
   duration <- as.difftime(stop - start, units = "secs")
 
+  time <- start
+
   repeat {
-    time <- Sys.time() - clockstart
+    if (is.null(by))
+      time <- (Sys.time() - clockstart)*speed
+    else
+      time <- time + by
+
     if (time > duration)
       break
     time <- as.numeric(time, units = "secs") + start
+
+    if (verbose)
+      cat("time = ", signif(time, 2), "\n")
+
     changedNodes <- gltf$settime(time, animation)
+    havenode <- -1
     if (length(changedNodes)) {
       if (method == "wholeScene") {
         rgl::clear3d()
@@ -175,8 +245,8 @@ playgltf <- function(gltf, animation = 0, start = times[1],
         }
         for (o in objs) {
           prim <- getPrim(gltf, o)
-          # if (o == "0:22")
-          #   browser()
+          weights <- prim$unique_weights
+          joints <- as.numeric(colnames(weights))
           if (method == "partialScene") {
             pop3d(tag = o)
             for (n1 in containingNodes[[o]]) {
@@ -192,13 +262,17 @@ playgltf <- function(gltf, animation = 0, start = times[1],
               id <- plot3d(newobj, add=TRUE)
             }
           } else if (method == "rigid") {
-            wrapper <- wrappers[[o]]
-            for (i in seq_along(wrapper)) {
-              par3d(userMatrix = forward[,,prim$usejoint + 1] %*% backward[,,prim$usejoint + 1], subscene = wrapper[i])
+            getMatrices(containingNodes[[o]][1])
+            subids <- wrappers[[o]]
+            for (i in seq_along(subids)) {
+              transform <- weightedTransform(joints, weights[i,], forward, backward)
+              par3d(userMatrix = transform, subscene = subids[i])
             }
           }
         }
       }
+      if (method %in% c("partialScene", "rigid"))
+        useSubscene3d(root)
       par3d(save)
       par3d(skipRedraw = TRUE)
     }
