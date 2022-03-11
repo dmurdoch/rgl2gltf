e10dc800a343cd1c2c18bfaa36e550e892254678
Duncan Murdoch <murdoch.duncan@gmail.com>
2022-03-11 07:53:54 -0500

    Warn about morph targets

--- R/as.rglscene.R
+++ R/as.rglscene.R
@@ -30,9 +30,8 @@ primToRglobj <- function(prim, skinnum, gltf, defaultmaterial = NULL, id = NULL,
   class(prim) <- "gltfPrimitive"
 
   if (!is.null(prim$targets)) {
-    print(prim)
-    # What are prim targets????
-    browser()
+    # These are morph targets (see https://www.khronos.org/registry/glTF/specs/2.0/glTF-2.0.html#morph-targets)
+    warning("morph targets are not supported")
   }
   mat <- gltf$getRglMaterial(prim$material)
 
